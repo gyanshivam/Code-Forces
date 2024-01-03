81999577e33a1d63b8ec868a244100fff516f0a6
Coding_Ultron <99073430+gyanshivam@users.noreply.github.com>
2024-01-03 13:44:07 +0530

    Update file14.cpp

--- file14.cpp
+++ file14.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    int stoppage;
+    int enters,exits;
+    int arr[1000];
+    int grt=0,cap=0;
+    
+    cin>>stoppage;
+    
+    for(int i=0; i<stoppage; i++)
+    {
+        cin>>exits>>enters;
+        cap=cap+(enters-exits);
+        arr[i]=cap;
+    }
+    for(int i=0; i<stoppage; i++)
+    {
+        if(arr[i]>grt)
+            grt=arr[i];
+    }
+    
+    cout<<grt;
+	return 0;
+}
